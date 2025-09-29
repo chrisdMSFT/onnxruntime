79bb8b2daa858120de8ab0e4188dbceae1815ea7
Chris Dern <chrisd@microsoft.com>
2025-09-29 15:35:19 -0600

    wip

---
+++ b.cmd
@@ -0,0 +1,1 @@
+cmake --build build --config Release --target onnxruntime_perf_test
---
+++ go-test.cmd
@@ -0,0 +1,1 @@
+install\onnxruntime_perf_test.exe -r 100 "X:\onnxruntime-testdata\modelzoo\test_squeezenet\model.onnx"
