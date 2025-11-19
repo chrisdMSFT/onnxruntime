71a5125092d9460a2b4a4b5c79e0d47b77c85f4a
Chris Dern <chrisd@microsoft.com>
2025-11-18 17:14:00 -0700

    flaten-perf-test

---
+++ go-c.cmd
@@ -0,0 +1,1 @@
+robocopy build\RelWithDebInfo\ "C:\Users\chrisd\OneDrive - Microsoft\onnxruntime_perf_test\2509-30\RelWithDebInfo" *.dll *.exe
---
+++ p-chrisd.cmd
@@ -0,0 +1,1 @@
+cmake chrisd -B build -DCPPWINRT_VERSION=2.0.250303.1 
